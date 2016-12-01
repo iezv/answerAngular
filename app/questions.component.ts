e2cf245dde90f27771b505142060e10cea3139c2
ie-zv <ie-zv@yandex.ru>
2016-12-01 23:24:59 +0200

    Chat sms

--- app/questions.component.ts
+++ app/questions.component.ts
@@ -9,54 +9,89 @@ import { Component } from '@angular/core';
 export class QuestionsComponent {
 	showHello = false;
 	showQues1 = true;
+	showQues1bad = true;
 	showQues2 = true;
-	showQues3yes = true;
-	showQues3no = true;
-	showQues4 = true;
-	showRes = true;
-	showExit = true;
-	
-	answer: Answer;
-	
-	
+	showQues2bad = true;
+
+	pushstar1_2 = false;
+	pushstar1_3 = false;
+	pushstar1_4 = false;
+	pushstar1_5 = false;
+
+	pushstar2_2 = false;
+	pushstar2_3 = false;
+	pushstar2_4 = false;
+	pushstar2_5 = false;
+
+	exit = true;
+		
 	public showQuestion1(){
-		this.showHello = true;
 		this.showQues1 = false;
 	}
-	public showQuestion2(firstname: HTMLInputElement, 
-		       lastname: HTMLInputElement, email:HTMLInputElement ){
-		this.showQues1 = true;
-		this.showQues2 = false;
-		console.log(`Adding data firstname: ${firstname.value} and lastname: ${lastname.value} and email: ${email.value}`);
-        this.answer = new Answer(firstname.value, lastname.value, email.value, null);
-	}
-	public showQuestion3(rad: string){
-		this.showQues2 = true;
-		this.showQues3yes = false;
-		
-	}
-	public showQuestion4(){
-		this.showQues3yes = true;
-		this.showQues3no = true;
-		this.showQues4 = false;
-	}
 
-	public result(){
-		this.showRes = false;
-		this.showQues4 = true;
+	public showQuestion1bad(){
+		this.showQues1bad = false;
 	}
-
-	public exit(){
-		this.showRes = true;
-		this.showExit = false;
-	}
-
 	
-}
-
-export class Answer {
-	constructor(firstname:string, lastname:string, email: string, ansrers:string[]) {
+    public showQuestion2(){
+		this.showQues2 = false;
 	}
+
+    public showQuestion2bad(){
+        this.showQues2bad = false;
+	}
+    
+    public exitandsave(){
+        this.exit = false;
+	}
+	
+    public pushstar_1_2(){
+     this.pushstar1_2 = true;
+     this.showQuestion1bad();
+    }
+
+    public pushstar_1_3(){
+     this.pushstar1_2 = true;
+     this.pushstar1_3 = true;
+     this.showQuestion1bad();
+    }
+    public pushstar_1_4(){
+     this.pushstar1_2 = true;
+     this.pushstar1_3 = true;
+     this.pushstar1_4 = true;
+     this.showQuestion2();
+    }
+    public pushstar_1_5(){
+     this.pushstar1_2 = true;
+     this.pushstar1_3 = true;
+     this.pushstar1_4 = true;
+     this.pushstar1_5 = true;
+     this.showQuestion2();
+    }
+
+    public pushstar_2_2(){
+     this.pushstar2_2 = true;
+     this.showQuestion2bad();
+    }
+
+    public pushstar_2_3(){
+     this.pushstar2_2 = true;
+     this.pushstar2_3 = true;
+     this.showQuestion2bad();
+    }
+    public pushstar_2_4(){
+     this.pushstar2_2 = true;
+     this.pushstar2_3 = true;
+     this.pushstar2_4 = true;
+     this.exitandsave();
+    }
+    public pushstar_2_5(){
+     this.pushstar2_2 = true;
+     this.pushstar2_3 = true;
+     this.pushstar2_4 = true;
+     this.pushstar2_5 = true;
+     this.exitandsave();
+    }
 }
 
 
